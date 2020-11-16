9d7865fd36535524f30a0fa63f0c957a4410f27d
Jacob Prebys <jakers991@gmail.com>
2020-11-16 12:17:00 -0600

    script working

---
+++ price_grabber.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup
+from pycoingecko import CoinGeckoAPI
+from datetime import datetime
+import json
+
+cg = CoinGeckoAPI()
+time = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+
+current_prices_usd = {coin['id']: coin['market_data']['current_price']['usd'] for coin in cg.get_coins()}
+
+with open('./prices.txt', 'a') as file:
+    json.dump({time: current_prices_usd}, file)
