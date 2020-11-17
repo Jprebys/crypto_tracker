ae96f7f9ae6b377ad75e7c3cb8cd89f21cfe625b
Jacob Prebys <jakers991@gmail.com>
2020-11-16 21:02:37 -0600

    fixed decoder bug

--- price_grabber.py
+++ price_grabber.py
@@ -9,16 +9,20 @@ time = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 # Get all info for top 50 coins
 coins = cg.get_coins()
 
-# Update decoder file with coin symbols/names
-with open('decoder.txt', 'r+') as file:
+# Parse old decoder file with coin symbols/names and get current codes
+with open('C:\\Users\\jaker\\Documents\\projects\\crypto_tracker\\src\\decoder.txt', 'r') as file:
     old_codes = json.load(file)        
     new_codes = {coin['symbol']: coin['id'] for coin in coins}   
+
+# Update decoder file with all new and old codes
+with open('C:\\Users\\jaker\\Documents\\projects\\crypto_tracker\\src\\decoder.txt', 'w') as file:
     json.dump({**old_codes, **new_codes}, file)
+
 
 # Get current prices for top 50 coins in USD
 current_prices_usd = {coin['symbol']: coin['market_data']['current_price']['usd'] for coin in coins}
 
 # Dump current price data
-with open('prices.txt', 'a') as file:
+with open('C:\\Users\\jaker\\Documents\\projects\\crypto_tracker\\src\\prices.txt', 'a') as file:
     file.write('\n')
     json.dump({time: current_prices_usd}, file)
